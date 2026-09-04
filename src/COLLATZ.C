6e93db8f7959866d0fe86d49c7ffa3c6eafc9549
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-09-05 07:47:51 +0900

    Collatz

--- src/COLLATZ.C
+++ src/COLLATZ.C
@@ -1,8 +1,17 @@
 #include <bigint.h>
+void BIP(BigInt *n, BigInt *m) {
+    /*BigInt Plus*/
+    for (int i = 0; i < BS64bB; i++) {
+        n->data[i]+=m->data[i];
+    }
+}
 void BILS(BigInt *n) {
     /*BigInt Left Shift*/
     for (int64_t i = (64 * BS64bB) - 1; i >= 0; i--) {
         if (n->data[i >> 6] & (1ULL << (i & 63))) {
+            if (i == 64 * BS64bB - 1) {
+                continue;
+            }
             n->data[(i+1) >> 6] |= (1ULL << ((i+1) & 63));
             n->data[i >> 6] &= ~(1ULL << (i & 63));
         }
